d0ffacecd9097d51caf25975acaa6e2c6b1c1a0b
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2021-06-01 23:40:24 -0300

    Add files via upload

---
+++ Aula6/src/main/java/controllers/App.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/app")
+public class App extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String titulo = request.getParameter("titulo");
+		
+		System.out.println(titulo);
+		
+		String nomes[] = {"Jonathan", "Beatriz", "Natan"};
+		
+		request.setAttribute("nomes", nomes);
+		
+		
+		//RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		//dispatcher.forward(request, response);
+		
+	}
+	
+}
